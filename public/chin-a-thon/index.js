63c70713cd9804d68be38054fb2ba9fba35fa2e4
clxxiii <earthnoartisjusteh@gmail.com>
2022-06-25 02:11:08 -0400

    tasks 1 2 and 4

--- public/chin-a-thon/index.js
+++ public/chin-a-thon/index.js
@@ -1,15 +1,16 @@
-let tasksDone = 1;
+let tasksDone = [1, 2, 4];
 
 let taskList = document.getElementsByClassName("goals")[0];
 console.log(taskList.rows);
 addDoneClasses();
 
 async function addDoneClasses() {
-	for (let i = 1; i <= tasksDone; i++) {
+	for (let i = 0; i <= tasksDone.length; i++) {
 		await new Promise((resolve) => {
 			setTimeout(resolve, 250);
 		});
-		let task = taskList.rows[i - 1];
+		let taskDone = tasksDone[i];
+		let task = taskList.rows[taskDone - 1];
 		task.classList.add("done");
 	}
 }
