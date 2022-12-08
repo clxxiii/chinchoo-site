065631876cc57f09de70dcccd417bf5ed8a504dd
clxxiii <earthnoartisjusteh@gmail.com>
2022-12-08 17:21:10 -0500

    cross off days 3 and 4

--- public/chin-a-thon/index.js
+++ public/chin-a-thon/index.js
@@ -1,4 +1,4 @@
-let tasksDone = [1, 2];
+let tasksDone = [ 1, 2, 3, 4 ];
 
 let taskList = document.getElementsByClassName("goals")[0];
 console.log(taskList.rows);
@@ -6,9 +6,7 @@ addDoneClasses();
 
 async function addDoneClasses() {
   for (let i = 0; i <= tasksDone.length; i++) {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 250);
-    });
+    await new Promise((resolve) => { setTimeout(resolve, 250); });
     let taskDone = tasksDone[i];
     let task = taskList.rows[taskDone - 1];
     task.classList.add("done");
